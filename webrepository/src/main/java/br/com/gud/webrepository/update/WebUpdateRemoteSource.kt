728110ac04b2abebe12e0c4dce0f8bbee478913b
Rafael Godinho <rafael.godinho@dextra-sw.com>
2019-08-12 08:00:13 -0300

    - Add url traffic permission

--- webrepository/src/main/java/br/com/gud/webrepository/update/WebUpdateRemoteSource.kt
+++ webrepository/src/main/java/br/com/gud/webrepository/update/WebUpdateRemoteSource.kt
@@ -7,7 +7,5 @@ import retrofit2.http.POST
 interface WebUpdateRemoteSource {
 
     @POST("Louvemos_cifrado/scripts/wb.php")
-    fun getContentUpdate(
-        @Body webUpdateRequestObject: WebUpdateRequestObject
-    ): Deferred<Any>
+    fun getContentUpdate(@Body webUpdateRequestObject: WebUpdateRequestObject): Deferred<Any>
 }
